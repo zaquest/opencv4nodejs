ddf8e52ce28a5cfd8024ab422b8db072c0816dd0
zaquest <web.elektro.net@gmail.com>
2025-03-10 14:12:57 +0700

    Remove constructor return type for FacemarkLBFParams
    This causes TS1093 “Type annotation cannot appear on a constructor declaration” error. In fact it does not seem like it was ever supported. https://github.com/Microsoft/TypeScript/issues/11588

--- typings/FacemarkLBFParams.d.ts
+++ typings/FacemarkLBFParams.d.ts
@@ -38,5 +38,5 @@ export class FacemarkLBFParams {
   treeDepth: number;
   treeN: number;
   readonly verbose: boolean;
-  constructor(params?: Partial<IFacemarkLBFParams>): FacemarkLBFParams;
+  constructor(params?: Partial<IFacemarkLBFParams>);
 }
